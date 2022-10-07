a9d60abba7d736a0a1e088feecea381adcc50821
42nado <daryllfortunado@gmail.com>
2022-10-07 09:45:21 +0800

    initial commit

---
+++ artster/index.js
@@ -0,0 +1,15 @@
+
+// how to initial a new project
+
+
+const figlet = require("figlet");
+const colors = require("colors");
+
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data.rainbow)
+});
